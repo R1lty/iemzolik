31d58ce1cbdc01381d3cc04a17e47936b72b1171
R1lty <tiv51tiv51@gmail.com>
2023-06-08 17:54:22 +0300

    adding temas block

--- scripts/teams-info.js
+++ scripts/teams-info.js
@@ -1,20 +1,30 @@
-const teams = [{
-  image:'images/new-royalflames.png',
-  player1:'R1lty',
-  player2:'NicKaJ'
-},{
-  image:'images/new-royalflames.png',
-  player1:'R1lty',
-  player2:'NicKaJ'
-}]
+const teams = [
+  {
+    image: 'images/new-royalflames.png',
+    player1: 'R1lty',
+    player2: 'NicKaJ'
+  },
+  {
+    image: 'images/dzuz.png',
+    player1: 'Lampo4ka',
+    player2: 'Rutra_FM'
+  },
+  {
+    image: 'images/team2.png',
+    player1: 'Lampo4ka',
+    player2: 'Rutra_FM'
+  }
+];
+
 let teamsHTML = '';
-teams.forEach((teams)=>{
-  teamsHTML+=`
-  <div class="image-area">
-        <img class="team-image" src="${teams.image}" alt="">
-      </div>
-      <div class="first-line">${teams.player1}</div>
-      <div class="second-line">${teams.player2}</div>
+teams.forEach(team => {
+  teamsHTML += `
+    <div class="teams-area">
+      <img class="pic" src="${team.image}" alt="">
+      <div class="line1">${team.player1}</div>
+      <div class="line2">${team.player2}</div>
+    </div>
   `;
 });
+
 document.querySelector('.js-teams-grid').innerHTML = teamsHTML;
