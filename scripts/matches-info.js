2a8b23e1ea0d8c7a173a2be3afea3d154df9bd4b
R1lty <tiv51tiv51@gmail.com>
2023-06-09 23:16:04 +0300

    change the schedule

--- scripts/matches-info.js
+++ scripts/matches-info.js
@@ -31,32 +31,12 @@ const gameinfo =[{
 },{
   team1:'',
   team2:'',
-  team1_image:'images/polinaFans.png',
-  team2_image:'images/jora.png',
-
-  group:'ГРУППА А',
-  score:'VS',
-  time:'21:00',
-  data:'10.06'
-},{
-  team1:'',
-  team2:'',
   team1_image:'images/jora.png',
   team2_image:'images/gvatar.png',
 
   group:'ГРУППА А',
   score:'VS',
-  time:'21:30',
-  data:'10.06'
-},{
-  team1:'',
-  team2:'',
-  team1_image:'images/new-royalflames.png',
-  team2_image:'images/polinaFans.png',
-
-  group:'ГРУППА А',
-  score:'VS',
-  time:'09:00',
+  time:'21:00',
   data:'10.06'
 },{
   team1:'',
