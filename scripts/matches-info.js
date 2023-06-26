a5933e540554c4b68256966a3612e6057dc2a644
R1lty <tiv51tiv51@gmail.com>
2023-06-26 22:24:07 +0300

    match update

--- scripts/matches-info.js
+++ scripts/matches-info.js
@@ -393,6 +393,17 @@ const gameinfo =[{
 },{
   team1:'',
   team2:'',
+  team1_image:'images/gvatar.png',
+  team2_image:'images/mvs-off.jpg',
+
+  group:'Play-off',
+  score:'VS',
+  time:'21:00',
+  tech_loss:"",
+  data:'27.06'
+},{
+  team1:'',
+  team2:'',
   team1_image:'images/kurushimi.png',
   team2_image:'images/snusitis.png',
 
@@ -412,18 +423,7 @@ const gameinfo =[{
   time:'23:00',
   tech_loss:"",
   data:'27.06'
-},{
-  team1:'',
-  team2:'',
-  team1_image:'images/gvatar.png',
-  team2_image:'images/mvs-off.jpg',
-
-  group:'Play-off',
-  score:'VS',
-  time:'21:00',
-  tech_loss:"",
-  data:'28.06'
-}
+},
 ]
 gameinfo.sort();
 gameinfo.reverse();
